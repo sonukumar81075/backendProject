27625b2b501d77a3005c5adb4588c0f69c2ff264
sonukumar81075 <sonusaini81075@gmail.com>
2025-05-05 14:53:34 +0530

    first commit

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("backend learning");
 });
+app.get("/login", (req, res) => {
+  res.send("login successfull!");
+});
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log("server is runing on this port 5000");
